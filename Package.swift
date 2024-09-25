c881a5223879e12937d7d57fe33e3d299cd5d752
Mark Bridges <mark@bridgetech.io>
2024-09-25 12:14:09 +0100

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UserDefaultsActor",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UserDefaultsActor",
+            targets: ["UserDefaultsActor"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UserDefaultsActor"),
+        .testTarget(
+            name: "UserDefaultsActorTests",
+            dependencies: ["UserDefaultsActor"]
+        ),
+    ]
+)
